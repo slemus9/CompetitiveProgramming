62fc51e4c1ecabc57381c08d648225bd5ad4b176
slemus9 <sebastian.slc9@gmail.com>
2019-10-24 23:28:35 -0500

    AKBAR problem completed

--- src/algorithms/graph/graphTraversal/problems/AKBAR.java
+++ src/algorithms/graph/graphTraversal/problems/AKBAR.java
@@ -1,78 +1,131 @@
 package algorithms.graph.graphTraversal.problems;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
-/**
- * https://www.spoj.com/problems/AKBAR/
- */
 public class AKBAR {
 
-    private static HashMap<Integer, ArrayList<Integer>> G;
-    private static HashMap<Integer, Integer> strenghts;
-    private static Queue<Integer> q;
-    private static boolean[] used;
-    private static int[] guarded;
+    private static Edge[] G;
+    private static int[] strengths;
+    private static int[] soldiers;
+    private static int[] q;
+    private static int[] used;
+    private static int[] distances;
+    private static int visited;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        IntIO in = new IntIO(System.in, 1<<14);
+        int t = in.getInt();
 
         for (int i = 0; i < t; i++) {
 
-            G = new HashMap<>();
-            strenghts = new HashMap<>();
-            q = new LinkedList<>();
+            visited = 0;
 
-            String[] nrm = br.readLine().split(" ");
-            int n = Integer.parseInt(nrm[0]);
-            int r = Integer.parseInt(nrm[1]);
-            int m = Integer.parseInt(nrm[2]);
+            int n = in.getInt(), r = in.getInt(), m = in.getInt();
+
+            G = new Edge[n];
+            soldiers = new int[m];
+            strengths = new int[m];
+            distances = new int[n];
 
             for (int j = 0; j < r; j++) {
-                String[] uv = br.readLine().split(" ");
-                int u = Integer.parseInt(nrm[0]);
-                int v = Integer.parseInt(nrm[1]);
-
-                if (G.containsKey(u)){
-                    G.get(u).add(v);
-                }else {
-                    ArrayList<Integer> adj = new ArrayList<>();
-                    adj.add(v);
-                    G.put(u, adj);
-                }
+                int u = in.getInt() - 1, v = in.getInt() - 1;
+                G[u] = new Edge(v, G[u]);
+                G[v] = new Edge(u, G[v]);
             }
 
             for (int j = 0; j < m; j++) {
-                String[] ks = br.readLine().split(" ");
-                int k = Integer.parseInt(ks[0]);
-                int s = Integer.parseInt(ks[1]);
-                strenghts.put(k, s);
+                int k = in.getInt() - 1, s = in.getInt();
+                soldiers[j] = k;
+                strengths[j] = s;
             }
 
+            q = new int[n];
+            used = new int[n];
+            boolean optimum = true;
 
-            used = new boolean[G.size()];
-            guarded = new int[G.size()];
+            Arrays.fill(used, -1);
+            Arrays.fill(distances, -1);
+
+            for (int j = 0; j < m; j++) {
+                optimum = bfs(soldiers[j], strengths[j]);
+                if (!optimum) break;
+            }
+
+            if (optimum && visited == n) System.out.println("Yes");
+            else System.out.println("No");
 
         }
     }
 
-    private static void bfs(int s){
-        q.add(s);
-        used[s] = true;
-        while (!q.isEmpty()){
-            int v = q.poll();
-            for(int u: G.get(v)){
-                if (!used[u]){
-                    used[u] = true;
-                    q.add(u);
+    private static boolean bfs(int s, int cities){
+        if (used[s] == -1){
+
+            int start = 0, end = 0;
+            q[end++] = s;
+            used[s] = s;
+            distances[s] = 0;
+            visited++;
+            boolean notRepeated = true;
+
+            while (start < end && notRepeated){
+                int v = q[start++];
+
+                if (used[v] != s && used[v] > -1){
+                    notRepeated = false;
+                }else {
+                    if (distances[v] < cities){
+                        for(Edge e = G[v]; e != null; e = e.dest){
+                            int u = e.to;
+                            if (used[u] == -1){
+                                used[u] = s;
+                                visited++;
+                                q[end++] = u;
+                                distances[u] = distances[v] + 1;
+                            }else {
+                                if (used[u] != s){
+                                    notRepeated = false;
+                                    break;
+                                }
+                            }
+
+                        }
+                    }
                 }
             }
+
+            return notRepeated;
+        }else {
+            return false;
+        }
+    }
+
+    private static class IntIO extends BufferedInputStream {
+        public IntIO(final InputStream in)
+        {
+            super(in);
+        }
+
+        public IntIO(final InputStream in, final int buf_size)
+        {
+            super(in, buf_size);
+        }
+
+        public int getInt() throws IOException {
+            int chr = read();
+            while(chr<=' ') chr = read();
+            int sum = chr-'0';
+            while((chr = read())>' ') sum = (sum<<3)+sum+sum + chr-'0';
+            return sum;
+        }
+    }
+
+    private static class Edge{
+        int to;
+        Edge dest;
+        Edge(int to, Edge dest){
+            this.to = to;
+            this.dest = dest;
         }
     }
 
