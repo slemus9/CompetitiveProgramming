fc175cd4bde48884db46ac698a6e0ac75611d1df
Sebastián Lemus <sebastian.slc9@gmail.com>
2019-11-13 23:53:55 -0500

    WATER problem updated

--- src/algorithms/graph/graphTraversal/problems/WATER.cpp
+++ src/algorithms/graph/graphTraversal/problems/WATER.cpp
@@ -13,10 +13,13 @@ struct point{
 };
 
 int n, m, t;
+int INF = 100010;
 const int MAX_SIZE = 110;
 vector<vector<int>> G;
-vector<vector<bool>> puddles;
 queue<point> q;
+bool visited [MAX_SIZE][MAX_SIZE];
+int deltaX [4] = {0, 0, -1, 1};
+int deltaY [4] = {1, -1, 0, 0};
 
 bool isValid(int x, int y){
     return  x >= 0 && x < m && y >= 0 && y < n;
@@ -26,8 +29,54 @@ bool isEdge(int x, int y){
     return  x == m - 1 || y == n - 1;
 }
 
-void bfs(point s){
 
+int bfs(point s){
+    visited[s.y][s.x] = true;
+    q.push(s);
+    bool leaked = false;
+    int vol = 0;
+
+    while (!q.empty() && !leaked){
+        point v = q.front();
+        q.pop();
+
+        int minHeight = INF;
+        for (int i = 0; i < 4; ++i) {
+            int ux = v.x + deltaX[i];
+            int uy = v.y + deltaY[i];
+
+            if (isValid(ux, uy) && !visited[uy][ux]){
+                int adjHeight = G[uy][ux];
+
+                if (G[v.y][v.x] >= adjHeight){
+                    visited[uy][ux] = true;
+
+                    if (isEdge(ux, uy)){
+                        leaked = true;
+                        break;
+                    } else{
+                        q.push((point) {ux, uy});
+                    }
+                } else{
+
+                    if (adjHeight < minHeight){
+                        minHeight = adjHeight;
+                    }
+                }
+            }
+        }
+        cout << minHeight << "-" << G[s.y][s.x] << endl;
+        vol = minHeight == INF ? 0 : minHeight - G[s.y][s.x];
+    }
+    return leaked ? 0 : vol;
+}
+
+void reloadVisited(){
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            visited[i][j] = false;
+        }
+    }
 }
 
 int main(){
@@ -35,17 +84,23 @@ int main(){
     for (int i = 0; i < t; ++i) {
         cin >> n >> m;
         G.resize(n);
-        puddles.resize(n);
 
         for (int j = 0; j < n; ++j) {
-            G.resize(m);
-            puddles.resize(m);
             for (int k = 0; k < m; ++k) {
-                cin >> G[i][j];
+                int h;
+                cin >> h;
+                G[j].push_back(h);
             }
         }
 
-
+        int accVol = 0;
+        for (int j = 1; j < n - 1; ++j) {
+            for (int k = 1; k < m - 1; ++k) {
+                reloadVisited();
+                accVol += bfs((point) {k, j});
+            }
+        }
+        cout << accVol << endl;
     }
 
     return 0;
