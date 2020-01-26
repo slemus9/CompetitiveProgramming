1d144c996b4df7f43f619c9c75b77ab27abe1d26
Sebastián Lemus <sebastian.slc9@gmail.com>
2020-01-26 13:37:37 -0500

    Completed 13 problems of 1000 - 1400. Puzzles problem solved

--- src/codeforces/exercises/Puzzles.kt
+++ src/codeforces/exercises/Puzzles.kt
@@ -7,9 +7,14 @@ import kotlin.math.min
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    val f = br.readLine().split(" ").map { it.toInt() }.sorted().zipWithNext()
-    println(f.fold(100000){acc, (x, y) ->
-        if (y == x) 100000
-        else min(acc, y - x) }
-    )
+    val f = br.readLine().split(" ").map { it.toInt() }.sorted()
+    var ans = 10000
+    var i = 0
+    var j = i + n - 1
+    while (j < m){
+        ans = min(ans, f[j] - f[i])
+        i++
+        j = i + n - 1
+    }
+    println(ans)
 }
