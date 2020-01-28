a208127e9c525a283de8ed7e256f3d18e610b901
Sebastián Lemus <sebastian.slc9@gmail.com>
2020-01-28 14:11:08 -0500

    Completed 25 problems of 1000 - 1400

---
+++ src/codeforces/exercises/VasyaAndSocks.kt
@@ -0,0 +1,10 @@
+package codeforces.exercises
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    println(n + (n - 1)/(m - 1))
+}
