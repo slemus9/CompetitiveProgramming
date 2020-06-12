38f180640eced4946cceb1f282e390bdacdf075d
Sebastian <sebastian.slc9@gmail.com>
2020-06-11 23:24:00 -0500

    Completing CFA mandatory A Purple problems from codeforces from JuniorTrainingRoadmap

---
+++ src/algorithms/graph/connectivity/UnionFind.kt
@@ -0,0 +1,30 @@
+package algorithms.graph.connectivity
+
+class QuickUnion (val n: Int) {
+    private val parent = (0 until n).toMutableList()
+    private val size = MutableList(n){1}
+    private var components = n
+
+    tailrec fun find (p: Int): Int = when(p){
+        parent[p] -> p
+        else -> find(parent[p])
+    }
+
+    fun connected (p: Int, q: Int): Boolean =
+        find(p) == find(q)
+
+    fun union (p: Int, q: Int) {
+        val rootP = find(p)
+        val rootQ = find(q)
+        if (rootP != rootQ) {
+            components--
+            if (size[rootP] < size[rootQ]) {
+                parent[rootP] = rootQ
+                size[rootQ] += size[rootP]
+            } else {
+                parent[rootQ] = rootP
+                size[rootP] += size[rootQ]
+            }
+        }
+    }
+}
